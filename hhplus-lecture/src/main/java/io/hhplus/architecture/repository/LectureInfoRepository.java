afdd5cd98278b95f36abb864ab2cb442da2d1713
ghdghdghd <ghdtmdgus3641@gmail.com>
2024-03-27 01:02:24 +0900

    update

--- hhplus-lecture/src/main/java/io/hhplus/architecture/repository/LectureInfoRepository.java
+++ hhplus-lecture/src/main/java/io/hhplus/architecture/repository/LectureInfoRepository.java
@@ -3,13 +3,16 @@ package io.hhplus.architecture.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import io.hhplus.architecture.domain.LectureInfo;
 import io.hhplus.architecture.dto.LectureResDTO;
 import io.hhplus.architecture.dto.ReserveResDTO;
+import jakarta.persistence.LockModeType;
 
 public interface LectureInfoRepository extends JpaRepository<LectureInfo, Long> {
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<LectureInfo> findByIdWithWriteLock(Long lectureId);
 
 //	ReserveResDTO selectByUserIdByAndLectureId(long userId, Long lectureId);
